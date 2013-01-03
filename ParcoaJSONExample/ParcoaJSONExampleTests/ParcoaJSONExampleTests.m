c08df370369cf140c89d01accec13dbad249ce12
James Brotchie <brotchie@gmail.com>
2013-01-03 12:23:59 +1000

    Separated examples into a separate project dependent on libParcoa.

--- ParcoaJSONExample/ParcoaJSONExampleTests/ParcoaJSONExampleTests.m
+++ ParcoaJSONExample/ParcoaJSONExampleTests/ParcoaJSONExampleTests.m
@@ -37,23 +37,5 @@
 
 @implementation ParcoaJSONExampleTests
 
-- (void)setUp
-{
-    [super setUp];
-    
-    // Set-up code here.
-}
-
-- (void)tearDown
-{
-    // Tear-down code here.
-    
-    [super tearDown];
-}
-
-- (void)testExample
-{
-    STFail(@"Unit tests are not implemented yet in ParcoaJSONExampleTests");
-}
 
 @end
